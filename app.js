a2a50a030c835c6cad623781c5f75e4b22f2453f
Greg-Mah <blaze1794@gmail.com>
2021-07-10 11:15:29 -0700

    created project

---
+++ app.js
@@ -0,0 +1,28 @@
+//State
+let gameState=
+{
+    
+    board:[]
+};
+
+function resetGame(state,height,width)
+{
+    state.board=[];
+    for(let i=0;i<state.board;i++)
+    {
+
+    }
+}
+
+//DOM Selectors
+
+//DOM Manupulation Functions
+function renderBoard(state)
+{
+    for(let i=0;i<state.board.length;i++)
+    {
+
+    }
+}
+
+//BootStrapping
